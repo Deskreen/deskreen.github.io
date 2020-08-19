65e6066a7f48db18234c9e41cfdfdbbee1d576bc
Pavlo Buidenkov <pavlobu@gmail.com>
2020-08-19 19:14:14 +0300

    try darkwire.io to connect to local websocket server

---
+++ precache-manifest.d9d51ce35856d73a940e44a59b4a85f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ad3af840c664cf7fdc251dc59974e6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9e127863aec6becb37b",
+    "url": "/static/css/2.d070a7d2.chunk.css"
+  },
+  {
+    "revision": "bcc95dad5656ca11f2ae",
+    "url": "/static/css/main.9e63bac6.chunk.css"
+  },
+  {
+    "revision": "a9e127863aec6becb37b",
+    "url": "/static/js/2.2fc2065e.chunk.js"
+  },
+  {
+    "revision": "bcc95dad5656ca11f2ae",
+    "url": "/static/js/main.401772f5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "82901651f589482a97f72edca06e135c",
+    "url": "/static/media/beep.82901651.mp3"
+  }
+]);
