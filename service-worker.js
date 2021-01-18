3b84bd268aaf30e1123e31704b27b15ea09c981a
Pavlo Buidenkov <pavlobu@gmail.com>
2021-01-18 02:05:31 +0200

    add links to github

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4644b9b98a61a4bb2d96829f07b02c88.js"
+  "/precache-manifest.774f76430256334670e1134c72d551c7.js"
 );
 
 self.addEventListener('message', (event) => {
