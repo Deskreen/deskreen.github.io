50d0f2eb19f1558c3c388fab03f357e1673ccf5f
Pavlo Buidenkov <pavlobu@gmail.com>
2025-11-02 13:13:47 +0100

    better adblock

--- _next/static/chunks/85948554cf8aa210.js
+++ _next/static/chunks/85948554cf8aa210.js
@@ -1,10 +1,10 @@
 __turbopack_load_page_chunks__("/_app", [
-  "static/chunks/fb3f1306a2344ba8.js",
+  "static/chunks/3539af80c20bf3e2.js",
   "static/chunks/775426fc8d7de5c2.js",
   "static/chunks/37042f03446b35f0.js",
   "static/chunks/fd5a59b0907bce37.js",
   "static/chunks/9375c7acc9785dff.js",
   "static/chunks/e14316df383b3ad7.css",
   "static/chunks/8e2a545e7046c238.css",
-  "static/chunks/turbopack-a49a75bc3f97aaff.js"
+  "static/chunks/turbopack-0d9fcbb21e9932b9.js"
 ])
