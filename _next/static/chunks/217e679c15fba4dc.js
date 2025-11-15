b9c8eae169880891e916d824a8c37f721bed2087
Pavlo Buidenkov <pavlobu@gmail.com>
2025-11-15 02:12:10 +0100

    lang

---
+++ _next/static/chunks/217e679c15fba4dc.js
@@ -0,0 +1,15 @@
+__turbopack_load_page_chunks__("/ui", [
+  "static/chunks/ae7f716099b73ebb.js",
+  "static/chunks/ccf98e58a24e7232.js",
+  "static/chunks/6a2c7909b4305d2c.js",
+  "static/chunks/4382f658efc22246.js",
+  "static/chunks/853de9ab9a0c8015.js",
+  "static/chunks/43b5178cb3629948.js",
+  "static/chunks/7c827604f430d8cd.js",
+  "static/chunks/5d85c03bcffbd940.js",
+  "static/chunks/780bd2b66b050aa9.js",
+  "static/chunks/d0534a99ba990c43.js",
+  "static/chunks/f863f82e5957e740.js",
+  "static/chunks/6474e502d8bdace7.js",
+  "static/chunks/turbopack-818d4281c44c2c62.js"
+])
---
+++ _next/static/chunks/8b7c271c169ffba6.js
@@ -0,0 +1,10 @@
+__turbopack_load_page_chunks__("/demos", [
+  "static/chunks/ae7f716099b73ebb.js",
+  "static/chunks/7c827604f430d8cd.js",
+  "static/chunks/0978d85096200286.js",
+  "static/chunks/5d85c03bcffbd940.js",
+  "static/chunks/780bd2b66b050aa9.js",
+  "static/chunks/d0534a99ba990c43.js",
+  "static/chunks/d0af9084ae2c6194.js",
+  "static/chunks/turbopack-4cf94313fee50851.js"
+])
