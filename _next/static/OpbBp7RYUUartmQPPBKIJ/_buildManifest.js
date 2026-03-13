bae8a9bff73b1b9d8846aed99a5199cd71d57ff0
Pavlo Buidenkov <pavlobu@gmail.com>
2026-03-13 16:06:21 +0100

    fix

---
+++ _next/static/OpbBp7RYUUartmQPPBKIJ/_buildManifest.js
@@ -0,0 +1,207 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/f4e61ab5c4ad8838.js"
+  ],
+  "/404": [
+    "static/chunks/efab0d542c8a2571.js"
+  ],
+  "/500": [
+    "static/chunks/454592dc9cf6163f.js"
+  ],
+  "/[localePref]": [
+    "static/chunks/e5faaba6f81ce5f7.js"
+  ],
+  "/[localePref]/500": [
+    "static/chunks/4346bd992279a188.js"
+  ],
+  "/[localePref]/about": [
+    "static/chunks/b0adcaa2fdc1a61c.js"
+  ],
+  "/[localePref]/about-us": [
+    "static/chunks/75a611d6248d4522.js"
+  ],
+  "/[localePref]/blog": [
+    "static/chunks/68dd2d9f9f363463.js"
+  ],
+  "/[localePref]/blog/[slug]": [
+    "static/chunks/944e1a8730da01f6.js"
+  ],
+  "/[localePref]/contact-us": [
+    "static/chunks/938c3530f4846c01.js"
+  ],
+  "/[localePref]/contribute": [
+    "static/chunks/de0986aba7160455.js"
+  ],
+  "/[localePref]/data-deletion": [
+    "static/chunks/cc45c62d2857c762.js"
+  ],
+  "/[localePref]/demos": [
+    "static/chunks/971a934049606a90.js"
+  ],
+  "/[localePref]/download": [
+    "static/chunks/c9cad902bcf960ba.js"
+  ],
+  "/[localePref]/download/compare": [
+    "static/chunks/b27927d4dd1acff8.js"
+  ],
+  "/[localePref]/downloads-ce": [
+    "static/chunks/c58a597b2bede608.js"
+  ],
+  "/[localePref]/faq": [
+    "static/chunks/754464b98b17f328.js"
+  ],
+  "/[localePref]/features": [
+    "static/chunks/0e1255584da0707b.js"
+  ],
+  "/[localePref]/get-in-touch": [
+    "static/chunks/b9281021fb0b973b.js"
+  ],
+  "/[localePref]/howto": [
+    "static/chunks/e144afb3851f8982.js"
+  ],
+  "/[localePref]/privacy": [
+    "static/chunks/c95ada78e6030284.js"
+  ],
+  "/[localePref]/privacy-policy": [
+    "static/chunks/9d16f71211cda14b.js"
+  ],
+  "/[localePref]/purpose": [
+    "static/chunks/579eb4af419234e4.js"
+  ],
+  "/[localePref]/terms-of-use": [
+    "static/chunks/4ffec0a4bc2bb5c7.js"
+  ],
+  "/[localePref]/ui": [
+    "static/chunks/8b55551e2d3e575c.js"
+  ],
+  "/[localePref]/watch/[videoId]": [
+    "static/chunks/493454d7c2ed8361.js"
+  ],
+  "/_error": [
+    "static/chunks/faef926fb7dc9b09.js"
+  ],
+  "/about": [
+    "static/chunks/d609c68f1c297ed0.js"
+  ],
+  "/about-us": [
+    "static/chunks/c342a164e5775830.js"
+  ],
+  "/blog": [
+    "static/chunks/f39369694e13ddf1.js"
+  ],
+  "/blog/[slug]": [
+    "static/chunks/966a25077760768d.js"
+  ],
+  "/contact-us": [
+    "static/chunks/7826659bcc830ab3.js"
+  ],
+  "/contribute": [
+    "static/chunks/ebb57bc99e5e0ee6.js"
+  ],
+  "/data-deletion": [
+    "static/chunks/74feaa2e87fd98be.js"
+  ],
+  "/demos": [
+    "static/chunks/250e92089176e3a4.js"
+  ],
+  "/download": [
+    "static/chunks/be594958c47b55d9.js"
+  ],
+  "/download/compare": [
+    "static/chunks/ed46be0f2ef8d603.js"
+  ],
+  "/downloads-ce": [
+    "static/chunks/802f156ae99c5f3e.js"
+  ],
+  "/faq": [
+    "static/chunks/db1175651edaeb8a.js"
+  ],
+  "/features": [
+    "static/chunks/8c172f5c47bc6d6a.js"
+  ],
+  "/get-in-touch": [
+    "static/chunks/e0d9d18914a9a957.js"
+  ],
+  "/howto": [
+    "static/chunks/9cd830b45b17ca40.js"
+  ],
+  "/privacy": [
+    "static/chunks/1c20e1df4e38f4d8.js"
+  ],
+  "/privacy-policy": [
+    "static/chunks/6097a05c22aae62e.js"
+  ],
+  "/purpose": [
+    "static/chunks/712497013f904b1d.js"
+  ],
+  "/team": [
+    "static/chunks/ddb9ed5fea8a7712.js"
+  ],
+  "/terms-of-use": [
+    "static/chunks/9fc98b88e092f6c4.js"
+  ],
+  "/ui": [
+    "static/chunks/2f68ea5170146b72.js"
+  ],
+  "/watch/[videoId]": [
+    "static/chunks/d0c21b02a586024d.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/404",
+    "/500",
+    "/_app",
+    "/_error",
+    "/about",
+    "/about-us",
+    "/api/hello",
+    "/blog",
+    "/blog/[slug]",
+    "/contact-us",
+    "/contribute",
+    "/data-deletion",
+    "/demos",
+    "/download",
+    "/download/compare",
+    "/downloads-ce",
+    "/faq",
+    "/features",
+    "/get-in-touch",
+    "/howto",
+    "/privacy",
+    "/privacy-policy",
+    "/purpose",
+    "/team",
+    "/terms-of-use",
+    "/ui",
+    "/watch/[videoId]",
+    "/[localePref]",
+    "/[localePref]/500",
+    "/[localePref]/about",
+    "/[localePref]/about-us",
+    "/[localePref]/blog",
+    "/[localePref]/blog/[slug]",
+    "/[localePref]/contact-us",
+    "/[localePref]/contribute",
+    "/[localePref]/data-deletion",
+    "/[localePref]/demos",
+    "/[localePref]/download",
+    "/[localePref]/download/compare",
+    "/[localePref]/downloads-ce",
+    "/[localePref]/faq",
+    "/[localePref]/features",
+    "/[localePref]/get-in-touch",
+    "/[localePref]/howto",
+    "/[localePref]/privacy",
+    "/[localePref]/privacy-policy",
+    "/[localePref]/purpose",
+    "/[localePref]/terms-of-use",
+    "/[localePref]/ui",
+    "/[localePref]/watch/[videoId]"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
